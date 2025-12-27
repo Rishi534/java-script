f648f816fdc5f0c772fdc786f055c84921ca35e6
Rishi534 <kumarrishimanjhi@gmail.com>
2025-12-27 12:16:06 +0000

    datatypes

---
+++ 1_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version 
+
+// alart(3+3) // we are using nodejs , not browser 
+
+console.log(2+2)
+
+console.log("Rishi")
+
+let name = "Rishi"
+let age = 18
+let isLoggedIn = false
+let state 
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => value is not assign 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Rishi") // string
+console.log(typeof age) // number
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
