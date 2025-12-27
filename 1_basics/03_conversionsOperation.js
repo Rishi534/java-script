02d1c91ddf2cbfcee9d45f5327518a5d16d91ceb
Rishi534 <kumarrishimanjhi@gmail.com>
2025-12-27 13:16:46 +0000

    conversion of datatypes

---
+++ 1_basics/03_conversionsOperation.js
@@ -0,0 +1,31 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+// null => 0
+// undefined => NaN
+// 
+
+let isLoggedIn = "Rishi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false 
+// "Rish" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
